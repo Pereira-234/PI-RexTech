43098cb0483019978ebe6c0a15ef6069a3b6212f
Arthur Medeiros Pinheiro <20231011110020@ifrn.local>
2025-09-22 17:48:20 -0300

    Detalhamento aperfeiçoado 2

--- projeto/rexapp/models/Imagem.py
+++ projeto/rexapp/models/Imagem.py
@@ -1,5 +1,18 @@
 from django.db import models
 from rexapp.models.Produto import Produto
+import os
+import uuid 
+
+
+def rename_image(instance, filename):
+    # Extract the file extension
+    ext = filename.split('.')[-1]
+    # Generate a new unique filename
+    new_filename = f"{uuid.uuid4()}.{ext}"
+    # Define the upload path
+    return new_filename
+
 class Imagem(models.Model):
-    imagem = models.ImageField(null = False, blank= True)
+    imagem = models.ImageField(null = False, blank= True, upload_to=rename_image)
     produto = models.ForeignKey(Produto, on_delete=models.RESTRICT, related_name='imagens', null=True, blank=True)
+
